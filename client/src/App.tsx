cf2718f9a8483d55cb52f819d33122f0cc42948c
victorspinei <topgspineivictor@gmail.com>
2024-08-22 19:18:08 +0300

    changed a word and rebuilt

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import TodoForm from './components/TodoForm'
 import TodoList from './components/TodoList'
 
 
-export const BASE_URL = import.meta.env.ENV === "development" ?
+export const BASE_URL = import.meta.env.MODE === "development" ?
   "http://localhost:5000/api" : "/api";
 
 function App() {
